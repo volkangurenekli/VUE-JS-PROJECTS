269f5f274ab8ad45581f51f2b29ec0e93d525a2c
VOLKAN GÜRENEKLİ <volkangurenekli@gmail.com>
2020-06-14 23:19:23 +0300

    added new abilities

--- VueResource/src/App.vue
+++ VueResource/src/App.vue
@@ -7,17 +7,21 @@
           <input type="text" class="form-control" v-model="userName" />
         </div>
         <button class="btn btn-primary" @click="save()">Save</button>
+        <button class="btn btn-primary" @click="saveCustom()">Save Custom</button>
+        <br />
+        <br />
+        <button class="btn btn-primary" @click="saveWithResource()">Save with Resource</button>
         <br />
         <br />
         <button class="btn btn-success" @click="get()">Get Datas</button>
+        <button class="btn btn-success" @click="getWithResource()">Get with Resource</button>
         <hr />
         <hr />
         <ul class="list-group">
           <li class="list-group-item" v-for="user in users" :key="user.id">
             {{ user.data.userName }}
-            <button class="btn btn-danger" @click="deleteUser(user.key)">
-              X
-            </button>
+            <button class="btn btn-danger" @click="deleteUser(user.key)">X</button>
+            <button class="btn btn-danger" @click="deleteWithResource(user.key)">delete</button>
           </li>
         </ul>
       </div>
@@ -30,36 +34,68 @@ export default {
   data() {
     return {
       userName: "",
-      users: [],
+      users: []
     };
   },
   methods: {
     save() {
       this.$http
         .post("users.json", {
-          userName: this.userName,
+          userName: this.userName
         })
-        .then((response) => console.log(response));
+        .then(response => console.log(response));
     },
     get() {
       this.users = [];
-      this.$http.get("users.json", {}).then((response) => {
+      this.$http.get("users.json", {}).then(response => {
         let data = response.data;
         for (let key in data) {
           this.users.push({
             key: key,
-            data: data[key],
+            data: data[key]
           });
         }
       });
     },
     deleteUser(key) {
-      this.$http.delete(`users/${key}.json`, {}).then((response) => {
+      this.$http.delete(`users/${key}.json`, {}).then(response => {
         console.log(response);
         this.get();
       });
     },
+    saveWithResource() {
+      this.$resource("users.json").save({}, { userName: this.userName });
+    },
+    deleteWithResource(key) {
+      console.log("deleteWithResource -> deleteWithResource", key);
+      this.$resource.delete(key);
+
+      this.$resource.delete("users/" + key + ".json");
+    },
+    getWithResource() {
+      this.users = [];
+      this.$resource("users.json")
+        .get()
+        .then(response => {
+          let data = response.data;
+          for (let key in data) {
+            this.users.push({
+              key: key,
+              data: data[key]
+            });
+          }
+        });
+    },
+    saveCustom() {
+      this.resource.kaydet({}, { userName: this.userName });
+    }
   },
+  created() {
+    const customActions = {
+      kaydet: { method: "POST", url: "users.json" }
+    };
+    this.resource = this.$resource("users.json", {}, customActions);
+  }
 };
 </script>
 
